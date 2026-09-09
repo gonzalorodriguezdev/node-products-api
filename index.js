9a2e91db742169ea8c7cd33497d71a595e43a8da
Gonzalo Rodriguez <gonzalorodriguezdev@gmail.com>
2026-09-09 18:23:59 -0300

    feat: complete products CRUD

--- index.js
+++ index.js
@@ -26,6 +26,16 @@ const servidor = http.createServer((req, res) => {
   } else if (req.url === "/productos" && req.method === "GET") {
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(productos));
+  } else if (partes[1] === "productos" && req.method === "DELETE") {
+    const indice = productos.findIndex((producto) => producto.id === id);
+    if (indice === -1) {
+      res.statusCode = 404;
+      return res.end("Producto no encontrado");
+    } else {
+      productos.splice(indice, 1);
+      res.statusCode = 200;
+      res.end("Producto eliminado");
+    }
   } else if (req.url === "/productos" && req.method === "POST") {
     let body = "";
 
@@ -34,27 +44,66 @@ const servidor = http.createServer((req, res) => {
     });
 
     req.on("end", () => {
-      const nuevoProducto = JSON.parse(body);
+      try {
+        const nuevoProducto = JSON.parse(body);
 
-      if (
-        typeof nuevoProducto.nombre !== "string" ||
-        typeof nuevoProducto.precio !== "number"
-      ) {
+        if (
+          typeof nuevoProducto.nombre !== "string" ||
+          typeof nuevoProducto.precio !== "number"
+        ) {
+          res.statusCode = 400;
+          return res.end("Datos invalidos");
+        }
+
+        console.log(nuevoProducto);
+        console.log(typeof nuevoProducto);
+
+        nuevoProducto.id = productos.length + 1;
+
+        productos.push(nuevoProducto);
+
+        res.statusCode = 201;
+        res.setHeader("Content-Type", "applicantion/json");
+        res.end(JSON.stringify(nuevoProducto));
+      } catch (error) {
         res.statusCode = 400;
-        return res.end("Datos invalidos");
+        return res.end("JSON inválido");
       }
-
-      console.log(nuevoProducto);
-      console.log(typeof nuevoProducto);
-
-      nuevoProducto.id = productos.length + 1;
-
-      productos.push(nuevoProducto);
-
-      res.statusCode = 201;
-      res.setHeader("Content-Type", "applicantion/json");
-      res.end(JSON.stringify(nuevoProducto));
     });
+  } else if (partes[1] === "productos" && req.method === "PUT") {
+    const indice = productos.findIndex((producto) => producto.id === id);
+    if (indice === -1) {
+      res.statusCode = 404;
+      return res.end("Producto no encontrado");
+    } else {
+      let body = "";
+
+      req.on("data", (chunk) => {
+        body += chunk;
+      });
+      req.on("end", () => {
+        try {
+          const datosActualizados = JSON.parse(body);
+
+          if (
+            typeof datosActualizados.nombre !== "string" ||
+            typeof datosActualizados.precio !== "number"
+          ) {
+            res.statusCode = 400;
+            return res.end("Datos inválidos");
+          }
+          productos[indice].nombre = datosActualizados.nombre;
+          productos[indice].precio = datosActualizados.precio;
+
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.end(JSON.stringify(productos[indice]));
+        } catch (error) {
+          res.statusCode = 400;
+          return res.end("JSON inválido");
+        }
+      });
+    }
   } else if (req.url === "/usuarios") {
     res.end("Lista de usuarios");
   } else if (productoEncontrado == null) {
